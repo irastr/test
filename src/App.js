de9484ea3bf82e2b533df61f6575f457e20b947a
ira strikun <ira.strikun@gmail.com>
2019-01-24 14:46:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./trading-hours";
 
 class App extends Component {
   render() {
+    console.log(data);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table className="table">
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Open/Close</th>
+          </tr>
+          {data.map((instrument, index) => {
+            return (
+              <tr key={index}>
+                <td>{instrument.instrumentID}</td>
+                <td>{instrument.name}</td>
+                <td>
+                  {instrument.tradingHours.some(item => {
+                    const timeFrom = new Date(item.from).toLocaleTimeString();
+                    const timeTo = new Date(item.to).toLocaleTimeString();
+                    const timeNow = new Date().toLocaleTimeString();
+                    return timeFrom < timeNow && timeTo > timeNow;
+                  })
+                    ? "Open"
+                    : "Close"}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
     );
   }
